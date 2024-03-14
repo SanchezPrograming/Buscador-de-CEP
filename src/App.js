1d21ba6ad24be8772ae2e6994bae1e946ddb74fd
Gustavo Sanchez <gussan2127@gmail.com>
2024-03-14 16:37:35 -0300

    .

--- src/App.js
+++ src/App.js
@@ -44,7 +44,6 @@ function App() {
          <main className='main'>
          <h2>CEP: {cep.cep}</h2>
          <span>{cep.logradouro}</span>
-         <span>{cep.complemento}</span>
          <span>{cep.bairro}</span>
          <span>{cep.localidade} -{cep.uf} </span>
    
